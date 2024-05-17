fdaa1383af2ba2d989451af6ee38ddeb03fe7284
balajich004 <balajichennupati123@gmail.com>
2024-05-17 12:59:48 +0530

    made some huge ui changes using dom and eventlisteners,Still working on making the new algorithm with dom efficient

---
+++ scripts/playRound.js
@@ -0,0 +1,31 @@
+function playRound(humanChoice,computerChoice){
+    humanChoice=humanChoice.toLowerCase();
+    if(humanChoice===computerChoice){
+        const wrapper=document.querySelector(".rps-wrapper");
+        if(humanChoice==="rock")
+        wrapper.innerHTML=`<button class="rock common"><img src="./images/rock-img.png" alt="rock-img"></button>`;
+        else if(humanChoice==="paper")
+            wrapper.innerHTML=`<button class="paper common"><img src="./images/paper-img.png" alt="rock-img"></button>`;
+        else if(humanChoice==="scissors")
+            wrapper.innerHTML=`<button class="scissor common"><img src="./images/scissors-img.png" alt="rock-img"></button>`;
+        result.innerHTML=`It's a tie!Your choice is ${humanChoice} and your opponents choice is also ${computerChoice}`;
+    }
+    else if((humanChoice=="rock" && computerChoice=="scissors") || (humanChoice=="scissors" && computerChoice=="paper") || (humanChoice=="paper" && computerChoice=="rock")){
+        if(humanChoice==="rock")
+            wrapper.innerHTML=`<button class="rock common"><img src="./images/rock-img.png" alt="rock-img"></button>
+        <button class="scissor common"><img src="./images/scissors-img.png" alt="rock-img"></button>`;
+        else if(humanChoice==="paper")
+            wrapper.innerHTML=`<button class="paper common"><img src="./images/paper-img.png" alt="rock-img"></button>
+        <button class="rock common"><img src="./images/rock-img.png" alt="rock-img"></button>`;
+        else if(humanChoice==="scissors")
+            wrapper.innerHTML=`<button class="scissor common"><img src="./images/scissors-img.png" alt="rock-img"></button>
+        <button class="paper common"><img src="./images/paper-img.png" alt="rock-img"></button>`;
+        
+        result.innerHTML=`You win!Your choice is ${humanChoice} and your opponents choice is ${computerChoice}`;
+    }
+    else if((humanChoice=="scissors" && computerChoice=="rock") || (humanChoice=="paper" && computerChoice=="scissors") || (humanChoice=="rock" && computerChoice=="paper")){
+    result.innerHTML=`You lose!Your choice is ${humanChoice} and your opponents choice is ${computerChoice}`;
+    }
+    else
+    result.innerHTML=`invalid choice 😾`;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 let humanScore=0,computerScore=0;
+const wrapper=document.querySelector(".rps-wrapper");
+const playerRock=document.querySelector(".rock");
+const playerPaper=document.querySelector(".paper");
+const playerScissors=document.querySelector(".scissors");
 
 function getComputerChoice(){
     let computerChoice;
@@ -12,35 +16,25 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-// function getHumanChoice(){
-//     let HumanChoice;
-//     HumanChoice=prompt("Enter your choice:");
-//     return HumanChoice;
-// }
 const result=document.querySelector(".result");
-// let humanSelection=getHumanChoice();
 let computerSelection=getComputerChoice();
-
-function playRound(humanChoice,computerChoice){
-    humanChoice=humanChoice.toLowerCase();
-    if(humanChoice===computerChoice)
-        result.innerHTML=`It's a tie!Your choice is ${humanChoice} and your opponents choice is ${computerChoice}`;
-    else if((humanChoice=="rock" && computerChoice=="scissors") || (humanChoice=="scissors" && computerChoice=="paper") || (humanChoice=="paper" && computerChoice=="rock")){
-        result.innerHTML=`You win!Your choice is ${humanChoice} and your opponents choice is ${computerChoice}`;
-    }
-    else if((humanChoice=="scissors" && computerChoice=="rock") || (humanChoice=="paper" && computerChoice=="scissors") || (humanChoice=="rock" && computerChoice=="paper")){
-    result.innerHTML=`You lose!Your choice is ${humanChoice} and your opponents choice is ${computerChoice}`;
-    }
-    else
-    result.innerHTML=`invalid choice 😾`;
-}
-
-const playerRock=document.querySelector(".rock");
-const playerPaper=document.querySelector(".paper");
-const playerScissors=document.querySelector(".scissors");
 playerRock.addEventListener("click",()=>{
-    getComputerChoice();
     playRound("rock",computerSelection);
 });
 playerPaper.addEventListener("click",()=>{playRound("paper",computerSelection)});
 playerScissors.addEventListener("click",()=>{playRound("scissors",computerSelection)});
+const replay=document.querySelector(".replay");
+replay.addEventListener("click",()=>{
+    wrapper.innerHTML=`<button class="rock common"><img src="./images/rock-img.png" alt="rock-img"></button>
+    <button class="paper common"><img src="./images/paper-img.png" alt="paper-img"></button>
+    <button class="scissors common"><img src="./images/scissors-img.png" alt="scissors-img"></button>`;
+    result.innerHTML="";
+    const playerRock=document.querySelector(".rock");
+    const playerPaper=document.querySelector(".paper");
+    const playerScissors=document.querySelector(".scissors");
+    playerRock.addEventListener("click",()=>{
+        playRound("rock",computerSelection);
+    });
+    playerPaper.addEventListener("click",()=>{playRound("paper",computerSelection)});
+    playerScissors.addEventListener("click",()=>{playRound("scissors",computerSelection)});
+});
